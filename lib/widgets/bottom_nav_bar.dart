ad80f442e5772cc6468d8cc63df91d0de107c031
KavinduLiyanage <sandeepa8956@gmail.com>
2020-09-28 23:03:03 +0530

    Implemented Dashboard

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../constants.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+      height: 80,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              kBackgroundColor1,
+              kBackgroundColor2,
+            ]
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          BottomNavItem(
+            title: "Home",
+            svgScr: "assets/icons/homeIcon.svg",
+            isActive: true,
+          ),
+          BottomNavItem(
+            title: "Discover",
+            svgScr: "assets/icons/heartIcon.svg",
+          ),
+          BottomNavItem(
+            title: "Me",
+            svgScr: "assets/icons/meIcon.svg",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final String svgScr;
+  final String title;
+  final Function press;
+  final bool isActive;
+  const BottomNavItem({
+    Key key,
+    this.svgScr,
+    this.title,
+    this.press,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SvgPicture.asset(
+            svgScr,
+            color: isActive ? kActiveIconColor : kTextColor,
+          ),
+          Text(
+            title,
+            style: TextStyle(color: isActive ? kActiveIconColor : kTextColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
